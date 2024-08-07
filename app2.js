525830ef64e5a2a6b19b29a030797610a196ecd7
maismas <102139078+maismas@users.noreply.github.com>
2024-08-07 12:58:31 +0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,4 +1,4 @@
-// Entry point for the apppppppppppppppppp
+// Entry point for the apppppppppppp
 const { exit } = require('process')
 const { logger, httpLogger } = require('./logger')
 const pjson = require('./package.json')
